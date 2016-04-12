99e7e83ea34076c4f3f142e31557c45713445e91
Aaron I. Clay <aaron.clay79@gmail.com>
2016-04-12 10:44:46 -0400

    Create Sales MAP.vb
    Map Of Sales.vb Form Code

---
+++ Sales MAP.vb
@@ -0,0 +1,77 @@
+-----------------------------
+** Sales.vb Form 4-12-2016 **
+-----------------------------
+
+
+Classes:
+------------
+
+
+------------
+**Internal**
+------------
+ PullNoresults - Private
+ ListSortAscending - Private
+ ListSortDescending - Private
+ 
+
+------------
+**External**
+------------
+ErrorLogging_V2  
+ScheduledActions  
+Sales_Performance_Report  
+TruncateNotes  
+DTPManiupulation  
+AF_And_JP_Logic  
+Issue_Leads  
+EmailIssueLeads  
+printToPrinterApptSheet  
+printToPrinterContactList  
+printToPrinterCustInfoSheet 
+bulkPrintOperations  
+emlTemplateLogic  
+convertLeadToStruct  
+ReusableListViewControl  
+CustomerHistory  
+Print_Sales_Perf_Report  
+
+
+
+--------------
+** Sub Forms**
+--------------
+
+Parent-
+	Main.vb
+
+Target-
+	Sales.vb
+
+Child-
+	SDResult
+	frmCreateList
+	SalesListBuilder
+	LogPhoneCall
+	CandCNotes
+	MemorizeNotes
+	ScheduleAction
+	exclusions
+	frmPrint
+	frmEmailTemplateChoice
+	frmEmailPreview
+	EmailTemplate
+	frmLinkSendEmail
+	frmEditSpecialInstructions
+	EditCustomerInfo
+	SetAppt_V2
+	
+
+--------------
+** Module **	
+--------------
+
+STATIC_VARIABLES
+
+
+ 
