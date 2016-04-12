a5797e1cf6a460aefbc0623ecda8f20ed5d5fba3
AIClay <aaron.clay79@gmail.com>
2016-04-12 07:04:43 -0400

    Example Merge
    What I did and why I did it here.

---
+++ Latest/tstMergeClass2.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Collections
+
+
+Public Class tstMergeClass2
+    Public Sub New()
+        Dim a As Integer = 0
+        For a = 0 To 30
+            MsgBox("Hello World. [Ubiquitous].", MsgBoxStyle.Information, "Hello.")
+        Next
+    End Sub
+End Class
