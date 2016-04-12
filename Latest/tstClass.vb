ac64eb8f6b950a5b5ce923a1bf789da985adc983
AIClay <aaron.clay79@gmail.com>
2016-04-12 06:22:21 -0400

    Test Class
    This is a test class to make sure merging is setting up correctly.

---
+++ Latest/tstClass.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO
+Imports System.Collections
+
+
+Public Class tstMerge
+
+    Public Sub New()
+        Dim a As Integer = 0
+        For a = 0 To 3
+            MsgBox("Hello World!", MsgBoxStyle.Information, "Hello World.")
+        Next
+    End Sub
+
+End Class
